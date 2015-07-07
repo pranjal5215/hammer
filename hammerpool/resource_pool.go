5e2d9795fdcb826fd7b7690d770d01f11365aee6
pranjal.pandit <pranjal.pandit@ibibogroup.com>
2015-07-07 16:58:50 +0530

    hammer pool

--- hammerpool/resource_pool.go
+++ hammerpool/resource_pool.go
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package pools provides functionality to manage and reuse resources
+// Package hammerpool provides functionality to manage and reuse resources
 // like connections.
-package pools
+package hammerpool
 
 import (
 	"errors"
